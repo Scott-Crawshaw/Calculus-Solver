853d0d577f8a3fc290edb95bdae37ec2f189124f
Jessica <jessicacheng99@gmail.com>
2020-02-27 23:02:39 -0500

    changed datastructures and organization in Calculate and added new law

--- README.md
+++ README.md
@@ -2,15 +2,15 @@
 ## Completed
 We have created appropriate data structures and have developed a parser that can parse even complex expressions into said data structures. The parser can work with binary operations such as +, -, *, ^, and /, as well as unary operations such as sin, cos, ln, -, and + (+ and - as prefixes).  
   
-We have also begun creating functions that can perform the steps needed to solve a given expression using a series of hard coded laws. Currently, we can successfully rewrite an expression given a law.
+We have also created functions that can perform the steps needed to solve a given expression using a series of hard coded laws. Currently, we can successfully rewrite an expression given a law and make the necessary substitutions to apply the law.  
 
 ## Need to Complete
-Unfortunately, there are some bugs in our calculus functions. For example, the function calculate never terminates. We need to repair this function. After that, we must implement configurable laws, deal with laws that involve constants, and implement an intelligent selection of laws (rather than randomly selecting any law that will work). We should have this done by Thursday evening.
+Currently, there is a method in Calculate.hs named compatible that will check if two substitutions made are compatible with each other before making said substitutions.  However, this method is still in progress and as of now, only returns true.  Right now, when testing our program on d/dx x^2, the program makes some matches it should not and applies the derivative of self law one too many times.
 
 ## Instructions
 To test our parser, simply run 'stack install' and then run the generated executable. You will be prompted to enter an expression, and will recieve a printed version of the expression parsed into data types.  
   
-To test our various calculus solving functions found in Calculate.hs, simply run 'stack ghci src/Calculate.hs'. In this file is the sum rule equation (testEqn) and a basic expression that the sum rule can be applied to (testExp). To test our rewrite function, which rewrites an expression given a law equation, run 'rewrites testEqn testExp'.
+To test our calculus solving functions found in Calculate.hs, simply run 'stack ghci src/Calculate.hs'. To test our derive function, which will apply laws to an expression, run 'derive laws testExp', where testExp is a hardcoded test expression.
 
 ## Authors
 Scott Crawshaw & Jessica Cheng 2020  
--- src/Calculate.hs
+++ src/Calculate.hs
@@ -19,17 +19,23 @@ derive :: [Law] -> Expr -> Calculation
 derive ls e = Calculation e (manyStep ls e)
 
 makeStep :: [Law] -> Expr -> [Step]
-makeStep ls e = [Step name (putItTogether e1 e2 e) | Law name (e1, e2) <- ls, length (removeBadEntries (getSubs e1 e)) /= 0]
+makeStep ls e = [Step name res | Law name (e1, e2) <- ls, res <- (putItTogether e1 e2 e)]
 
 -- Function only returns lists of size 1, which is probably not right
-rewrites :: Equation -> Expr -> Expr
-rewrites (e1, e2) exp = applyAll (getSubs e1 exp) e2
 
-callFuncs :: Expr -> Expr -> [(Expr, [(Expr, Expr)])] -> Expr
-callFuncs exp e2 ((part, ls):xs) = insertE2 exp (helpApply ((part, ls):xs) e2) part
-
-putItTogether :: Expr -> Expr -> Expr -> Expr
-putItTogether e1 e2 exp = callFuncs exp e2 (removeBadEntries (getSubs e1 exp))
+-- iSSUE (before meeting): should only be returning expression if expr 3 exists in expr 1
+-- FIX: put it in a list so can return empty list
+putItTogether :: Expr -> Expr -> Expr -> [Expr]
+putItTogether e1 e2 exp
+   = [apply sub e2 | sub <- match e1 exp] ++
+     case exp of
+         (BinOp op left right) -> [BinOp op left' right | left' <- putItTogether e1 e2 left] ++
+                                  [BinOp op left right' | right' <- putItTogether e1 e2 right]
+         -- TODO for other constructors (I did this part)
+         (Unary op exp) -> [Unary op exp' | exp' <- putItTogether e1 e2 exp]
+         (Deriv var exp) -> [Deriv var exp' | exp' <- putItTogether e1 e2 exp]
+         -- base case for constant, variable
+         _ -> []
 
 insertE2 :: Expr -> Expr -> Expr -> Expr
 insertE2 (BinOp op left right) modifiedE2 partExp
@@ -48,27 +54,18 @@ insertE2 (Const i) modifiedE2 partExp
     | partExp == (Const i) = modifiedE2
     | otherwise = Const i
 
-helpApply :: [(Expr, [(Expr, Expr)])] -> Expr -> Expr
-helpApply ((_, ls):_) e2 = writeE2 e2 ls
-
-writeE2 :: Expr -> [(Expr, Expr)] -> Expr
-writeE2 e2 [] = e2
-writeE2 e2 ((v, e):subs) = writeE2 (apply (v,e) e2) subs
-
-applyAll :: [(Expr, [(Expr, Expr)])] -> Expr -> Expr
-applyAll [] e = e
-applyAll ((_ , ((v, e):_)):subs) e2 = applyAll subs (apply (v, e) e2)
-
-testSubs :: [(Expr, Expr)]
+testSubs :: Subst
 testSubs = [(Var 'x',Var 'x'),(Var 'a',Const 2),(Var 'b',Unary Ln (Var 'x'))]
 
-apply :: (Expr, Expr) -> Expr -> Expr
-apply (v, e) (BinOp op left right) = BinOp op (apply (v, e) left) (apply (v, e) right)
-apply (v, e) (Unary op exp) = Unary op (apply (v, e) exp)
-apply (v, e) (Deriv var exp) = Deriv (apply (v, e) var) (apply (v, e) exp)
-apply (v, e) (Var c)
+-- ideally takes String bc only going to be substituting for variables
+apply :: Subst -> Expr -> Expr
+apply subst (BinOp op left right) = BinOp op (apply subst left) (apply subst right)
+apply subst (Unary op exp) = Unary op (apply subst exp)
+apply subst (Deriv var exp) = Deriv (apply subst var) (apply subst exp)
+apply ((v,e):subst) (Var c)
     | v == (Var c) = e
-    | otherwise = (Var c)
+    | otherwise = apply subst (Var c)
+apply [] x = x
 apply _ (Const i) = (Const i)
 
 removeBadEntries :: [(Expr, [(Expr, Expr)])] -> [(Expr, [(Expr, Expr)])]
@@ -77,29 +74,25 @@ removeBadEntries ((main, ls):subs)
     | length ls == 0 = removeBadEntries subs
     | otherwise = (main, ls) : removeBadEntries subs
 
-getSubs :: Expr -> Expr -> [(Expr, [(Expr, Expr)])]
-getSubs e1 (BinOp op left right) = ((BinOp op left right), match e1 (BinOp op left right)) :
-                                getSubs e1 left ++ getSubs e1 right
-getSubs e1 (Unary op exp) = ((Unary op exp), match e1 (Unary op exp)) : getSubs e1 exp
-getSubs e1 (Deriv var exp) = ((Deriv var exp), match e1 (Deriv var exp)) : 
-                                getSubs e1 var ++ getSubs e1 exp
-getSubs e1 (Var c) = ((Var c), match e1 (Var c)) : []
-getSubs e1 (Const i) = ((Const i), match e1 (Const i)) : []
-
-match :: Expr -> Expr -> [(Expr, Expr)]
-match (Deriv varL expL) (Deriv varE expE) = match varL varE ++ match expL expE
+-- ISSUE(before meeting w Joosten): returned empty list of substitutions
+-- FIX: return empty list bc no possible sobstitutions
+match :: Expr -> Expr -> [Subst]
+match (Deriv varL expL) (Deriv varE expE) = [l ++ r | l<-match varL varE, r<-match expL expE,compatible l r]
 match (BinOp opL leftL rightL) (BinOp opE leftE rightE)
-    | opL == opE = match leftL leftE ++ match rightL rightE
+    | opL == opE = [l ++ r | l<-match leftL leftE, r<- match rightL rightE,compatible l r]
     | otherwise = []
 match (Unary opL expL) (Unary opE expE)
     | opL == opE = match expL expE
     | otherwise = []
-match (Var l) exp = [(Var l, exp)]
+match (Var l) exp = [[(Var l, exp)]]
 match _ _ = []
+
+compatible :: Subst -> Subst -> Bool
+compatible _ _ = True -- TODO
 
 -- test structures
 testExp :: Expr
-testExp = (BinOp Mul (BinOp Pow (Var 'x') (Const 2)) (Deriv (Var 'x') (BinOp Mul (Const 2) (Unary Ln (Var 'x')))))
+testExp = (Deriv (Var 'x') (BinOp Pow (Var 'x') (Const 2)))
 
 testE1 :: Expr
 testE1 = (Deriv (Var 'x') (BinOp Mul (Var 'a') (Var 'b')))
